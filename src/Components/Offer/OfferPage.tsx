2ca6b3ede6df2f34b00a515d4919de5616ea1ec1
turzhzyk <andrewurbro@yahoo.com>
2026-03-06 14:49:27 +0100

    added offer json store

--- src/Components/Offer/OfferPage.tsx
+++ src/Components/Offer/OfferPage.tsx
@@ -1,43 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./OfferPage.module.css";
 import Offer from "../Offer";
 
-export const OfferItem = () => {
+export const OfferItem = ({
+  title,
+  desc,
+  number,
+  price,
+  url,
+}: {
+  title: string;
+  desc: string;
+  number: string;
+  price: string;
+  url: string;
+}) => {
   return (
     <div className={styles.item + " outline"}>
       <div className={styles.imgContainer}>
-        <img src="/bizhub.png" />
+        <img src={"/products/" + url} />
       </div>
       <div className={styles.text}>
-        <h2 className={styles.title}>Item name</h2>
-        <p className={styles.desc}>123123rdas</p>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.desc}>{desc}</p>
         <div className="flex justify-between">
-          <p className={styles.catalogNumber}>4400</p>
-          <strong className={styles.price}>20zł/zestaw</strong>
+          <p className={styles.catalogNumber}>{number}</p>
+          <strong className={styles.price}>{price}</strong>
         </div>
       </div>
     </div>
   );
-};  
+};
 
 export default function OfferPage() {
+  const [products, setProducts] = useState<any>(null);
+  useEffect(() => {
+    const loadPrices = async () => {
+      const res = await fetch("/config/products.json");
+      const data = await res.json();
+      setProducts(data);
+    };
+
+    loadPrices();
+  }, []);
+  if (products == null) return <div>Loading...</div>;
+  console.log(products.szklanki);
   return (
     <div className="w-full pt-30">
       <h1> Oferta dla restauracji</h1>
       <div className={styles.wrapper}>
-        <div className={styles.left + " offer-wrapper"}><Offer/></div>
+        <div className={styles.left + " offer-wrapper"}>
+          <Offer />
+        </div>
         <div className={styles.right}>
           <div> </div>
-          <div className={styles.rightWrapper}>
-            {" "}
-            <OfferItem />
-            <OfferItem />
-            <OfferItem />
-            <OfferItem />
-            <OfferItem />
-            <OfferItem />
-            <OfferItem />
-          </div>
+          <div className={styles.rightWrapper}>{products.szklanki.map((i)=>{
+            return <OfferItem title={i.name} desc={i.desc} url={i.img} price={i.price} number={i.catalogNumber}/>
+          })}</div>
         </div>
       </div>
     </div>
